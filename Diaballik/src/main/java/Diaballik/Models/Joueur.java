bb212003e7b9fab85a2ccee3f2eb42c3d14b3f1f
JohanDa9u <wassimayari10@gmail.com>
2020-05-05 20:22:54 +0200

    classe joueur

--- Diaballik/src/main/java/Diaballik/Models/Joueur.java
+++ Diaballik/src/main/java/Diaballik/Models/Joueur.java
@@ -1,6 +1,18 @@
 package Diaballik.Models;
 
+public class Joueur{
+    public TypeJoueur n; //joueur1, 2 ou IA
+    public PieceType couleur; //sa couleur
+    public int nbMove;
+    public int passe_faite;
+    public Joueur(TypeJoueur n, PieceType couleur, int nbMove, int passe_faite){
+        this.n = n;
+        this.couleur = couleur;
+        this.nbMove = nbMove;
+        this.passe_faite = passe_faite;
+    }
+}
 
-public enum Joueur {
+enum TypeJoueur {
     Joueur1, Joueur2, IA
 }
