5ce6216a270ae631646a02abaa8b597e9e1cdcae
DOLIPRANE74 <t.noellardin@gmail.com>
2020-05-05 14:16:59 +0200

    Fixation de la sortie

--- Diaballik/src/main/java/Diaballik/Vue/msgBox.java
+++ Diaballik/src/main/java/Diaballik/Vue/msgBox.java
@@ -12,9 +12,10 @@ public class msgBox {
         int result = JOptionPane.showConfirmDialog(frame, msg, titre, JOptionPane.YES_NO_OPTION, 
         		JOptionPane.WARNING_MESSAGE);
         if (result == JOptionPane.YES_OPTION) {
-        	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        	/*frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         	i.dispose();
-        	i.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            i.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);*/
+            System.exit(0);
         } 
         else if (result == JOptionPane.NO_OPTION) {
         	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
