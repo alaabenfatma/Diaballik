514c21dd98de19396c6a48b85f470f2f48e6f28d
DOLIPRANE74 <t.noellardin@gmail.com>
2020-05-07 17:37:08 +0200

    Ajout de la fonction WinningMove

--- Diaballik/src/main/java/Diaballik/Models/Jeu.java
+++ Diaballik/src/main/java/Diaballik/Models/Jeu.java
@@ -435,6 +435,22 @@ public class Jeu extends Observable {
         }
     }
 
+    public ArrayList<Position> winningMove(Piece p){
+        ArrayList<Position> list = p.passesPossibles();
+        ArrayList<Position> res = new ArrayList<Position>();
+        if(list.size()==0){return null;}
+        else{
+            for(int i=0;i<list.size();i++){
+                if(list.get(i).l == 0 && p.Type == PieceType.White){
+                    res.add(list.get(i));
+                }
+                if(list.get(i).l == 6 && p.Type == PieceType.Black){
+                    res.add(list.get(i));
+                }
+            }
+            return res;
+        }
+    }
     public static void main(String[] args) {
         Jeu j = new Jeu();
         j.test_Random_IA_IA(j.tr);
