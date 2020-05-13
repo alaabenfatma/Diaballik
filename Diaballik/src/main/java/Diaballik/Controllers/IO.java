5ef9a54570f0157ea8b9a63200724a287a62bfbd
Alaa <9027148+alaabenfatma@users.noreply.github.com>
2020-05-13 23:59:03 +0200

    terrain to char

--- Diaballik/src/main/java/Diaballik/Controllers/IO.java
+++ Diaballik/src/main/java/Diaballik/Controllers/IO.java
@@ -1,5 +1,38 @@
 package Diaballik.Controllers;
 
+import org.json.simple.JSONObject;
+
+import Diaballik.Models.Jeu;
+import Diaballik.Models.PieceType;
+
 public class IO {
-    
+    public static void ExportGameToJSON(Jeu j){
+        String player1 = j.joueur1.name;
+        String player2 = j.joueur2.name;
+        char[][] pieces = new char[7][7];
+        for (int i = 0; i < 7; i++) {
+            for (int k = 0; k < 7; k++) {
+                if(j.tr._terrain[i][k].Type==PieceType.White){
+                    if(j.tr._terrain[i][k].HasBall){
+                        pieces[i][k] = '1';
+                    }
+                    else{
+                        pieces[i][k] = 'W';
+                    }
+                }
+                else if(j.tr._terrain[i][k].Type==PieceType.Black){
+                    if(j.tr._terrain[i][k].HasBall){
+                        pieces[i][k] = '0';
+                    }
+                    else{
+                        pieces[i][k] = 'B';
+                    }
+                }
+                else{
+                    pieces[i][k] = ' ';
+                }
+            }
+        }
+        
+    }
 }
