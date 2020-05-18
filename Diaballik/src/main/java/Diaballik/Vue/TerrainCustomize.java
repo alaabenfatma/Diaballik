89fe7ab95e20079acd20cea8cd8bd690e132fe6d
Alaa <9027148+alaabenfatma@users.noreply.github.com>
2020-05-18 13:27:46 +0200

    ok terrain : bugs

--- Diaballik/src/main/java/Diaballik/Vue/TerrainCustomize.java
+++ Diaballik/src/main/java/Diaballik/Vue/TerrainCustomize.java
@@ -2,21 +2,28 @@ package Diaballik.Vue;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 import javax.swing.*;
 
 import com.bric.colorpicker.ColorPicker;
 import com.bric.colorpicker.listeners.ColorListener;
 import com.bric.colorpicker.models.ColorModel;
 import java.awt.Color;
+import java.awt.*;
 import Diaballik.Models.ConfigJeu;
 
 public class TerrainCustomize {
     ConfigJeu cfg;
     JFrame frame = new JFrame();
+    JPanel board;
 
     public TerrainCustomize(ConfigJeu _cfg) {
+
         this.cfg = _cfg;
+        board = new boardZone(cfg);
         final ColorPicker colorPicker1 = new ColorPicker(true, true);
         colorPicker1.setColor(Color.BLUE);
         final ColorPicker colorPicker2 = new ColorPicker(true, true);
@@ -36,7 +43,9 @@ public class TerrainCustomize {
                     @Override
                     public void colorChanged(ColorModel colorModel) {
                         color1.setBackground(colorPicker1.getColor());
-                        cfg.damierA = String.format("#%02x%02x%02x", colorPicker1.getColor().getRed(), colorPicker1.getColor().getGreen(), colorPicker1.getColor().getBlue());  
+                        cfg.damierA = String.format("#%02x%02x%02x", colorPicker1.getColor().getRed(),
+                                colorPicker1.getColor().getGreen(), colorPicker1.getColor().getBlue());
+                        board.repaint();
                     }
                 });
 
@@ -56,25 +65,88 @@ public class TerrainCustomize {
                     @Override
                     public void colorChanged(ColorModel colorModel) {
                         color2.setBackground(colorPicker2.getColor());
-                        cfg.damierB = String.format("#%02x%02x%02x", colorPicker2.getColor().getRed(), colorPicker1.getColor().getGreen(), colorPicker1.getColor().getBlue());  
+                        cfg.damierB = String.format("#%02x%02x%02x", colorPicker2.getColor().getRed(),
+                                colorPicker1.getColor().getGreen(), colorPicker1.getColor().getBlue());
+                        board.repaint();
                     }
                 });
 
             }
         });
 
-        JButton ok = new JButton();
+        final  JButton ok = new JButton();
         frame.setSize(400, 400);
-        color1.setBounds(400 - 80, 0, 40, 40);
-        color2.setBounds(400 - 80, 50, 40, 40);
-        ok.setBounds(400 - 80, 400 - 80, 40, 40);
-        JPanel board = new JPanel();
-        board.setBounds(0, 0, 400, 300);
 
+        frame.addComponentListener(new ComponentAdapter() {
+            public void componentResized(ComponentEvent evt) {
+                board.setLocation(0, 50);
+                color1.setBounds((frame.getWidth() / 2) - 40, 0, 40, 40);
+                color2.setBounds((frame.getWidth() / 2) + 40, 0, 40, 40);
+                ok.setBounds(frame.getWidth() - 40, 0, 40, 40);
+            }
+        });
         frame.add(color1);
         frame.add(color2);
         frame.add(ok);
         frame.add(board);
         frame.setVisible(true);
+       
+    }
+
+}
+
+class boardZone extends JPanel {
+    ConfigJeu cfg;
+
+    Graphics2D drawable;
+
+    protected boardZone(ConfigJeu _j) {
+        setBounds(0, 50, 400, 350);
+        cfg = _j;
+    }
+
+    protected void tracerDamier(Graphics2D drawable, Color couleur, int x, int y, int l, int h) {
+        drawable.setColor(couleur);
+        drawable.fillRect(x, y, l, h);
+    }
+
+    void drawSpecimen(Graphics2D g) {
+        // Tracé du plateau
+
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+
+        drawable = (Graphics2D) g;
+
+        // On efface tout
+        drawable.clearRect(0, 0, this.getWidth(), this.getHeight());
+        int largeurCase = this.getWidth() / 7;
+        int hauteurCase = this.getHeight() / 7;
+        // On prend des cases carrées
+
+        // Tracé du plateau
+        for (int ligne = 0; ligne < 7; ligne++) {
+            for (int colonne = 0; colonne < 7; colonne++) {
+                int x = colonne * largeurCase;
+                int y = ligne * hauteurCase;
+                // int marque = n.marque(ligne, colonne);
+                // Tracé du sol
+                if (ligne % 2 == 0) {
+                    if (colonne % 2 == 0) {
+                        tracerDamier(drawable, Color.decode(cfg.damierA), x, y, largeurCase, hauteurCase);
+                    } else {
+                        tracerDamier(drawable, Color.decode(cfg.damierB), x, y, largeurCase, hauteurCase);
+                    }
+                } else {
+                    if (colonne % 2 == 0) {
+                        tracerDamier(drawable, Color.decode(cfg.damierB), x, y, largeurCase, hauteurCase);
+                    } else {
+                        tracerDamier(drawable, Color.decode(cfg.damierA), x, y, largeurCase, hauteurCase);
+                    }
+                }
+            }
+        }
     }
 }
