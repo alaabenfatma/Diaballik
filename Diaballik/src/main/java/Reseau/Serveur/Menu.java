a52b4cdde6ea4a5423d161382b9c54d2d51116b0
yohan landreaud <y.landreaud@hotmail.fr>
2020-05-19 13:55:23 +0200

    menu finit

--- Diaballik/src/main/java/Reseau/Serveur/Menu.java
+++ Diaballik/src/main/java/Reseau/Serveur/Menu.java
@@ -1,29 +1,33 @@
 package Reseau.Serveur;
 
 import javax.swing.*;
+
+import Diaballik.Models.ConfigJeu;
+
 import java.awt.Color;
+import java.awt.event.*;
 
 public class Menu {
 
-    public Menu(){
-        JFrame frame = new JFrame();
-        JPanel panel = new JPanel();
-        JLabel timer = new JLabel("Timer");
+    public Menu(final ConfigJeu C){
+        final JFrame frame = new JFrame();
+        final JPanel panel = new JPanel();
+        final JLabel timer = new JLabel("Timer");
         panel.setLayout(null);
         frame.setSize(515, 410);
         frame.setLocationRelativeTo(null);
         frame.setTitle("Parametrage de la partie");
-        JButton Unemin = new JButton("1min");
-        JButton Deuxmin = new JButton("2min");
-        JButton Troismin = new JButton("3min");
-        JButton illimite = new JButton("illimite");
-        JButton joueur1 = new JButton("joueur1");
-        JButton joueur2 = new JButton("joueur2");
-        JLabel n1 = new JLabel("nom du joueur 1 :");
-        JTextArea nom1 = new JTextArea("joueur1");
-        JLabel n2 = new JLabel("nom du joueur 2 :");
-        JTextArea nom2 = new JTextArea("joueur2");
-        JLabel premier = new JLabel("Joue en premier");
+        final JButton Unemin = new JButton("1min");
+        final JButton Deuxmin = new JButton("2min");
+        final JButton Troismin = new JButton("3min");
+        final JButton illimite = new JButton("illimite");
+        final JButton joueur1 = new JButton("joueur1");
+        final JButton joueur2 = new JButton("joueur2");
+        final JButton jouer = new JButton("jouer");
+        final JButton retour = new JButton("retour");
+        final JLabel n1 = new JLabel("nom du joueur 1 :");
+        final JTextArea nom1 = new JTextArea("joueur1");
+        final JLabel premier = new JLabel("Joue en premier");
         timer.setBounds(230,10,100,40);
         illimite.setBounds(20,50,100,40);
         Unemin.setBounds(140,50,100,40);
@@ -34,6 +38,8 @@ public class Menu {
         premier.setBounds(200,180,100,40);
         joueur1.setBounds(120,220,100,40);
         joueur2.setBounds(280,220,100,40);
+        jouer.setBounds(280,280,100,40);
+        retour.setBounds(120,280,100,40);
 
         illimite.setBackground(Color.pink);
         joueur1.setBackground(Color.pink);
@@ -49,8 +55,91 @@ public class Menu {
         panel.add(premier);
         panel.add(joueur1);
         panel.add(joueur2);
+        panel.add(jouer);
+        panel.add(retour);
         frame.setContentPane(panel);
         frame.setVisible(true);
-    }
+
+        
+
+        illimite.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                illimite.setBackground(Color.pink);
+                Unemin.setBackground(null);
+                Deuxmin.setBackground(null);
+                Troismin.setBackground(null);
+                C.setTimer(Diaballik.Models.ConfigJeu.Timer.illimite);
+            }
+        });
     
+        Unemin.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                Unemin.setBackground(Color.pink);
+                illimite.setBackground(null);
+                Deuxmin.setBackground(null);
+                Troismin.setBackground(null);
+                C.setTimer(Diaballik.Models.ConfigJeu.Timer.un);
+            }
+        });
+    
+        Deuxmin.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                Deuxmin.setBackground(Color.pink);
+                Unemin.setBackground(null);
+                illimite.setBackground(null);
+                Troismin.setBackground(null);
+                C.setTimer(Diaballik.Models.ConfigJeu.Timer.deux);
+            }
+        });
+    
+        Troismin.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                Troismin.setBackground(Color.pink);
+                Unemin.setBackground(null);
+                Deuxmin.setBackground(null);
+                illimite.setBackground(null);
+                C.setTimer(Diaballik.Models.ConfigJeu.Timer.trois);
+            }
+        });
+
+        joueur1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                joueur1.setBackground(Color.pink);
+                joueur2.setBackground(null);
+                C.setP1First(true);
+            }
+        });
+
+        joueur2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                joueur2.setBackground(Color.pink);
+                joueur1.setBackground(null);
+                C.setP1First(false);
+            }
+        });
+
+        jouer.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                frame.setVisible(false);
+                Diaballik.Models.Jeu j = new Diaballik.Models.Jeu(); 
+                String nom = nom1.getText();
+                C.setName1(nom);       
+                j.configurer(C);
+                j.start();
+                Diaballik.Vue.CollecteurEvenements control = new Diaballik.Controllers.ControleurMediateur(j);
+		        Diaballik.Vue.Plateau.demarrer(j, control, C);
+            }
+        });
+
+        retour.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                frame.setVisible(false);
+            }
+        });
+
+
+    }  
+
+    
+
 }
--- Diaballik/src/main/java/Reseau/Serveur/Partie.java
+++ Diaballik/src/main/java/Reseau/Serveur/Partie.java
@@ -16,43 +16,14 @@ public class Partie implements Runnable{
 
     public static void paraPartie(Diaballik.Models.ConfigJeu C){
         C.setMode(Diaballik.Models.ConfigJeu.Mode.humain);
-        Menu menu = new Menu();
-		Scanner saisie = new Scanner(System.in);
-		System.out.println("Veuillez saisir la durée du timer : 1min, 2min, 3min, illimite");
-		String timer = saisie.next();
-		System.out.println("Veuillez saisir le nom du joueur1 :");
-		String name1 = saisie.next();
-		System.out.println("Veuillez saisir le nom du joueur2 :");
-		String name2 = saisie.next();
-		System.out.println("Veuillez saisir j1 si le joueur1 joue en premier, j2 sinon :");
-        String premier = saisie.next();
-
-        if(timer == "1min"){
-            C.setTimer(Diaballik.Models.ConfigJeu.Timer.un);
-        }else if(timer == "2min"){
-            C.setTimer(Diaballik.Models.ConfigJeu.Timer.deux);
-        }else if(timer == "3min"){
-            C.setTimer(Diaballik.Models.ConfigJeu.Timer.trois);
-        }else if(timer == "illimite"){
-            C.setTimer(Diaballik.Models.ConfigJeu.Timer.illimite);
-        }
-        C.setName1(name1);
-        C.setName2(name2);
-        if(premier == "j1"){
-            C.setP1First(false);
-        }else {
-            C.setP1First(true);
-        }
+        Menu menu = new Menu(C);
     }
     
     public void run() {
         Diaballik.Models.Jeu j = new Diaballik.Models.Jeu();
         Diaballik.Models.ConfigJeu C = new Diaballik.Models.ConfigJeu();
         paraPartie(C);
-        j.configurer(C);
-        j.start();
-		Diaballik.Vue.CollecteurEvenements control = new Diaballik.Controllers.ControleurMediateur(j);
-		Diaballik.Vue.Plateau.demarrer(j, control, C);
+		
     }
     
     
