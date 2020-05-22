a9d37cf2caa042005361c3b8c414244042da1aeb
Alaa Ben Fatma <9027148+alaabenfatma@users.noreply.github.com>
2020-05-22 22:01:50 +0200

    Add files via upload

---
+++ Diaballik/src/main/java/Diaballik/IA_vs_IA.java
@@ -0,0 +1,37 @@
+package Diaballik;
+
+import Diaballik.Models.*;
+import Diaballik.Models.IA.*;
+
+public class IA_vs_IA {
+    public static void main(String[] args) {
+        int white = 0, black = 0;
+        for (int i = 0; i < 30; i++) {
+            Jeu j = new Jeu();
+            j.configurer(new ConfigJeu());
+            j.config.setVariante(true);
+            j.init();
+            j.start();
+            IA_easy easy = new IA_easy(j);
+            MiniMax hard = new MiniMax(j, PieceType.White);
+            MiniMax moyen = new MiniMax(j, PieceType.Black);
+            while (Math.abs(Evaluator.scoreOfBoard(j.tr)) != 9999) {
+               
+                //Black
+                //easy.joueTourIAEasy();
+                hard.VanillaMiniMax(new State(j), 3, true);
+                //j.JoueTourIAMiniMax(hard.bestMove);
+                //White
+                moyen.VanillaMiniMax(new State(j), 1, false);
+                j.JoueTourIAMiniMax(moyen.bestMove);
+            }
+            if (Evaluator.scoreOfBoard(j.tr) > 0) {
+                white++;
+            } else {
+                black++;
+
+            }
+        }
+        System.out.printf("Black wins : %d\nWhite wins : %d", black, white);
+    }
+}
